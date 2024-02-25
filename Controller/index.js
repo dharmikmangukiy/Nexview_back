f4d852eb423e9117a658210f40135d158b72d5ce
dharmikmangukiy <dharmikmangukiya2@gmail.com>
2024-02-25 18:50:00 +0530

    first commit

---
+++ Controller/index.js
@@ -0,0 +1,12 @@
+import loginController from "./Auth/loginController";
+import productController from "./productController";
+import welcomeController from "./welcomeController";
+import registerController from "./Auth/registerController";
+
+
+export {
+  loginController,
+  productController,
+  welcomeController,
+  registerController
+};
