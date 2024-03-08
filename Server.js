122c0248db3fc298310e4ce19016730eb2c92f2a
LeminGhori <ghorilemin@gmail.com>
2024-03-08 13:58:25 +0530

    132

--- Server.js
+++ Server.js
@@ -22,9 +22,9 @@ mongoose.connect(DB_URL, {
 }).catch((error) => {
   console.error("Error connecting to DB:", error);
 });
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(routes);
 app.use(welcome);
